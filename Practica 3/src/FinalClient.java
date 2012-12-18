b193f28ead0bc5aa905a9ab758d360f0d93dccd8
demil133 <demil133@gmail.com>
2012-12-18 19:21:50 +0100

    Formateado el codigo y comentado

--- Practica 3/src/FinalClient.java
+++ Practica 3/src/FinalClient.java
@@ -1,36 +1,34 @@
 import java.io.*; import java.net.*;
 public class FinalClient {
-    public static void main (String args[]) {
-	System.out.println("main");
+	public static void main (String args[]) {
 
-	String host = "localhost", theLine;
-	//if (args.length > 0) { host=args[0]; }
-	//System.out.println(""+ host);
+		String host = "localhost", theLine;
+		//if (args.length > 0) { host=args[0]; }
+		//System.out.println(""+ host);
 
-	int port = FinalServer.CONNECTION_PORT;
-	if(args.length > 0) port = Integer.parseInt(args[0]);
-	try {
-	    Socket s = new Socket(host,port);
-	    System.out.println("socket: "+s.isConnected());
+		int port = FinalServer.CONNECTION_PORT;
+		if(args.length > 0) port = Integer.parseInt(args[0]);
+		try {
+			Socket s = new Socket(host,port);
+			System.out.println("CLIENTE CONECTADO : "+ s.getInetAddress().toString() + ":" + s.getPort());
 
-	    LineNumberReader netIn = new LineNumberReader(
-		    new InputStreamReader(s.getInputStream()));
-	    PrintWriter netOut = new PrintWriter(s.getOutputStream(), true);
-	    LineNumberReader sysIn = new LineNumberReader(
-		    new InputStreamReader(System.in));
-	    while (true) {
-		System.out.print(">>");
-		theLine = sysIn.readLine();
-		System.out.print("<<");
-		if (theLine.equals(".")){
-		    break;
-		}
-		netOut.println(theLine);
-		System.out.println(netIn.readLine());
-	    }
-	} catch (UnknownHostException e) { System.out.println(e);
-	} catch (IOException e) {System.out.println(e);}
-	System.out.println("fin");
+			LineNumberReader netIn = new LineNumberReader( new InputStreamReader(s.getInputStream()));
+			PrintWriter netOut = new PrintWriter(s.getOutputStream(), true);
+			LineNumberReader sysIn = new LineNumberReader( new InputStreamReader(System.in));
+			
+			while (true) {
+				System.out.print(">>");
+				theLine = sysIn.readLine();
+				System.out.print("<<");
+				if (theLine.equals(".")){
+					break;
+				}
+				netOut.println(theLine);
+				System.out.println(netIn.readLine());
+			}
+		} catch (UnknownHostException e) { System.out.println(e);
+		} catch (IOException e) {System.out.println(e);}
+		System.out.println("fin");
 
-    }
+	}
 }
